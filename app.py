8956f5ede145929f63a18528e54c98a163f51aa8
Emerson Gregório <emersonlgregorio@gmail.com>
2021-08-16 21:57:13 -0400

    Aplicando Bootstrap nos Formularios

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
 lista_usuarios = ['Emerson', 'Francieli', 'João', 'Fulano', 'Morfeu']
 vagas = ['Auxiliar de Produção', "Mecânico de Maquinas Pesadas", 'Especialista de TI', 'Auxiliar Contábil']
+
+app.config['SECRET_KEY'] = 'c1444b3dc88d5bb19c4d917edb7d780d'
 
 @app.route('/')
 def home():
@@ -23,6 +26,13 @@ def usuarios():
 @app.route('/trabalhe-conosco')
 def trabalhe_conosco():
     return render_template('trabalhe_conosco.html', vagas=vagas)
+
+
+@app.route('/login')
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Email, DataRequired, Length, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação de senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
+    botao_submit_login = SubmitField('Fazer login')
