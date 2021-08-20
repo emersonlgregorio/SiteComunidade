d77e9d9af03ab9b0b4e9d87cb698736f56fdb876
Emerson Gregório <emersonlgregorio@gmail.com>
2021-08-19 21:16:54 -0400

    Relacionamento ente Posts e Usuarios

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
 from app import database
 from datetime import datetime
 
+
 class Usuario(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, default='default.jpg', nullable=False)
+    posts = database.relationship('Post', backref='autor', lazy=True)
+    cursos = database.Cloumn(database.String, nullable=False, default='Não Informado')
 
 
 class Post(database.Model):
@@ -14,4 +17,4 @@ class Post(database.Model):
     titulo = database.Column(database.String, nullable=False)
     corpo = database.Column(database.Text, nullable=False)
     data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
-
+    id_usuario = database.Column(database.Integer, database.ForeingKey('usuario.id'), nullable=False)
