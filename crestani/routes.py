928b982cbb323ddba4dd9dc6a56ee53bf4875702
Emerson Gregório <emersonlgregorio@gmail.com>
2021-08-29 13:25:13 -0400

    Permitir a Criação de Post

--- crestani/routes.py
+++ crestani/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, redirect, request, flash
 from crestani import app, database, bcrypt
-from crestani.forms import FormCriarConta, FormLogin, FormEditarPerfil
-from crestani.models import Usuario
+from crestani.forms import FormCriarConta, FormLogin, FormEditarPerfil, FormCriarPost
+from crestani.models import Usuario, Post
 from flask_login import login_user, logout_user, current_user, login_required
 import secrets
 import os
@@ -79,10 +79,19 @@ def perfil():
     return render_template('perfil.html', foto_perfil=foto_perfil)
 
 
-@app.route('/post/criar')
+@app.route('/post/criar', methods=['GET', 'POST'])
 @login_required
 def criar_post():
-    return render_template(('criarpost.html'))
+    form = FormCriarPost()
+    if form.validate_on_submit():
+        post = Post(titulo=form.titulo.data,
+                    corpo=form.corpo.data,
+                    autor=current_user)
+        database.session.add(post)
+        database.session.commit()
+        flash('Post criado com sucesso', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('criarpost.html', form=form)
 
 
 
